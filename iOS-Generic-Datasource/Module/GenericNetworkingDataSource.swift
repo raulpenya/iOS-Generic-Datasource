4b172941181b7d4503fcc1d0541a4572fe2e1fbb
rpenya <rpenya.mail@gmail.com>
2023-02-06 13:53:52 +0100

    project created

---
+++ iOS-Generic-Datasource/Module/GenericNetworkingDataSource.swift
@@ -0,0 +1,8 @@
+//
+//  GenericNetworkingDataSource.swift
+//  iOS-Generic-Datasource_Example
+//
+//  Created by raulbot on 6/2/23.
+//
+
+import Foundation
---
+++ iOS-Generic-Datasource_Example/iOS_Generic_Datasource_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Generic_Datasource_ExampleApp.swift
+//  iOS-Generic-Datasource_Example
+//
+//  Created by raulbot on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Generic_Datasource_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
