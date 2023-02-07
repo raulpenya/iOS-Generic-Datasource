04528f09dc1e7dff1ca198f42fcbb4035fd017c4
rpenya <rpenya.mail@gmail.com>
2023-02-07 12:44:47 +0100

    first request method added

--- iOS-Generic-Datasource/Module/GenericNetworkingDataSource.swift
+++ iOS-Generic-Datasource/Module/GenericNetworkingDataSource.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Combine
+
+struct Resource<T: Decodable, Q> {
+    let request: URLRequest
+    let transform: (T) -> Q
+}
+
+protocol GenericNetworkingDataSource {
+    func request<T, Q>(with session: URLSession, resource: Resource<T, Q>) async throws -> Q
+    func request<T, Q>(with session: URLSession, resource: Resource<T, Q>) -> AnyPublisher<Q, Error>
+}
+
+extension GenericNetworkingDataSource {
+    func request<T, Q>(with session: URLSession, resource: Resource<T, Q>) async throws -> Q {
+        let (data, _) = try await session.data(for: resource.request)
+        let decoder = JSONDecoder()
+        let entity = try decoder.decode(T.self, from: data)
+        return resource.transform(entity)
+    }
+    
+//    func request<T, Q>(with session: URLSession, resource: Resource<T, Q>) -> AnyPublisher<Q, Error> {
+//        let task = session.dataTask(with: resource.request)
+//        let task = session.dataTaskPublisher(for: <#T##URL#>)
+//        task.
+//    }
+}
