6c2979abffbce955582e5ac0b43cbe2c8bdf8160
rpenya <rpenya.mail@gmail.com>
2023-02-24 13:30:07 +0100

    RequestAwaitAsyncTests & RequestCombineTests added

---
+++ iOS-Generic-Datasource/UnitTests/RequestAwaitAsyncTests.swift
@@ -0,0 +1,14 @@
+//
+//  RequestAwaitAsyncTests.swift
+//  iOS-Generic-Datasource-Tests
+//
+//  Created by raulbot on 24/2/23.
+//
+
+import XCTest
+
+final class RequestAwaitAsyncTests: XCTestCase {
+
+    
+
+}
---
+++ iOS-Generic-Datasource/UnitTests/RequestCombineTests.swift
@@ -0,0 +1,13 @@
+//
+//  RequestCombineTests.swift
+//  iOS-Generic-Datasource-Tests
+//
+//  Created by raulbot on 24/2/23.
+//
+
+import XCTest
+
+final class RequestCombineTests: XCTestCase {
+
+    
+}
