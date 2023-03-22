e11e5e78404b733e6683edd75e4ef4a668de083e
rpenya <rpenya.mail@gmail.com>
2023-03-22 10:03:59 +0100

    GenericNetworkingDataSource code improved

--- iOS-Generic-Datasource/Module/DataSourceErrors.swift
+++ iOS-Generic-Datasource/Module/DataSourceErrors.swift
@@ -11,6 +11,8 @@ public enum DataSourceErrors: Int, Error {
     case networkingRequestError = -1
     case instanceException = 800
     case castHTTPURLResponseException = 801
+    case requestException = 802
+    case parametersException = 803
     public var localizedDescription: String {
         switch self {
         case .networkingRequestError:
@@ -19,6 +21,10 @@ public enum DataSourceErrors: Int, Error {
             return "DataSourceErrors :: Instance exception"
         case .castHTTPURLResponseException:
             return "DataSourceErrors :: Casting HTTPURLResponse exception"
+        case .requestException:
+            return "DataSourceErrors :: URLRequest exception"
+        case .parametersException:
+            return "DataSourceErrors :: Parameters exception"
         }
     }
     public var code: Int { return rawValue }
--- iOS-Generic-Datasource/Module/GenericNetworkingDataSource.swift
+++ iOS-Generic-Datasource/Module/GenericNetworkingDataSource.swift
@@ -14,19 +14,25 @@ struct Resource<T: Decodable, Q> {
 }
 
 protocol GenericNetworkingDataSource: AnyObject {
-    func request<T, Q>(with session: Session, resource: Resource<T, Q>) async throws -> Q
-    func request<T, Q>(with session: URLSession, resource: Resource<T, Q>) -> AnyPublisher<Q, Error>
+    func request<T, Q>(with session: Session, resource: Resource<T, Q>?) async throws -> Q
+    func request<T, Q>(with session: Session, resource: Resource<T, Q>?) -> AnyPublisher<Q, Error>
 }
 
 extension GenericNetworkingDataSource {
-    func request<T, Q>(with session: Session, resource: Resource<T, Q>) async throws -> Q {
+    func request<T, Q>(with session: Session = URLSession.shared, resource: Resource<T, Q>?) async throws -> Q {
+        guard let resource = resource else {
+            throw DataSourceErrors.requestException
+        }
         let (data, response) = try await session.data(for: resource.request)
         let responseData = try handleResponse(data: data, response: response)
         let entity = try JSONDecoder().decode(T.self, from: responseData)
         return resource.transform(entity)
     }
     
-    func request<T, Q>(with session: URLSession, resource: Resource<T, Q>) -> AnyPublisher<Q, Error> {
+    func request<T, Q>(with session: Session = URLSession.shared, resource: Resource<T, Q>?) -> AnyPublisher<Q, Error> {
+        guard let resource = resource else {
+            return Fail(error: DataSourceErrors.requestException).eraseToAnyPublisher()
+        }
         return session.dataTaskPublisher(for: resource.request).tryMap { [weak self] data, response in
             guard let strongSelf = self else {
                 throw  DataSourceErrors.instanceException
